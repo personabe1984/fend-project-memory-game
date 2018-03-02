9dfd74835a354d689692303b1634b4401fa9903f
Hans Choi <hans@Hanss-MBP.fios-router.home>
2018-03-03 07:26:37 +0800

    feat: The card will now can be flipped with just click of a mouse

--- js/app.js
+++ js/app.js
@@ -44,19 +44,6 @@ function shuffle(array) {
 
 
 /*
-$(".deck").click(function(){
-  alert( "Handler for .click() called." );
-  $(this).toggleClass("card open show");
-});
-//When the card is clicked I want it to turn and show the other side of the card.
-function flipCard(){
-    $(".card").click(function(){
-      alert( "Handler for .click() called." );
-      $(this).toggleClass("card open show");
-    });
-}
-*/
-/*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
@@ -66,13 +53,9 @@ function flipCard(){
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+var card = deck.children();
 
-
-
-
-
-
-
-
-
- 
+card.click(function(){
+  //alert( "Handler for .click() called." );
+  $(this).toggleClass("open show");
+});
